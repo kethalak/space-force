692052e45c952b433aa383d6c24d2d90f945af48
Octopal <roldan@thecosmicwhale.com>
2018-07-04 14:58:25 -0600

    Up to Chapter 2 on Bolt's Advanced Tutorial documentation

---
+++ Assets/_SpaceForce!/_Scenes/BoltTesting/scripts/PlayerCallbacks.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using Bolt.AdvancedTutorial;
+
+[BoltGlobalBehaviour("Level2")]
+public class PlayerCallbacks : Bolt.GlobalEventListener
+{
+
+	public override void SceneLoadLocalDone(string map)
+	{
+		print("called");
+		// this just instantiates our player camera,
+		// the Instantiate() method is supplied by the BoltSingletonPrefab<T> class
+		OnShipPlayerCamera.Instantiate();
+	}
+}
---
+++ Assets/_SpaceForce!/_Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	public float movementSpeed = .2f;
+	
+	void FixedUpdate () 
+	{
+		transform.position += Vector3.right * Input.GetAxis("Horizontal") * movementSpeed;
+		transform.position += Vector3.up * Input.GetAxis("Vertical") * movementSpeed;
+
+		Vector3 playerForward = transform.forward;
+		Vector3 playerRight = Vector3.Cross(Vector3.forward, playerForward);
+		transform.position += playerRight * Input.GetAxis("Horizontal") * movementSpeed;
+		transform.position += playerForward * Input.GetAxis("Vertical") * movementSpeed;
+	}
+}
